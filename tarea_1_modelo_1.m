6a4c06848cc4b7ba93c5b33f338a1d67c43c5889
Johann Aguayo <johann200_cardenas788@hotmail.com>
2023-01-29 21:51:48 -0600

    Agregando Edos de Cb

--- tarea_1_modelo_1.m
+++ tarea_1_modelo_1.m
@@ -3,20 +3,22 @@
 function Reactor_en_serie_1
 clear
 clc
-[T,y]=ode45(@modelo_1,[0,80],[0,0]);
-y(:,2)
+[T,y]=ode45(@modelo_1,[0,80],[0,0,0,0]);
+y(:,3)
 %datos
-plot(T,y(:,2),"r")
+plot(T,y)
 title("Tarea no.1 modelo 1 Aguayo Johann")
 ylabel("Ca [mol/m^3]"),xlabel("Tiempo [s]");
 grid
-legend("CA_total")
+legend("Ca1","Cb1","Ca2","Cb2")
 
 end
 function dydT=modelo_1(T,y)
 %datos
 Ca1 = y(1);
-Ca2 = y(2);
+Cb1 = y(2);
+Ca2 = y(3);
+Cb2 = y(4);
 F = 0.008; %m^3/s
 v = 0.04; %m^3
 k = 0.01; %1/s
@@ -24,6 +26,9 @@ Cao = 0.5; %mol/m^3
 
 %Ecuacion de diseño CSTR
 dydT(1)=((F*Cao)/v)-((F*Ca1)/v)-(k*Ca1);
-dydT(2)=((F*Ca1)/v)-((F*Ca2)/v)-(k*Ca2);
+dydT(2)=(k*Ca1)-((F*Cb1)/v);
+dydT(3)=((F*Ca1)/v)-((F*Ca2)/v)-(k*Ca2);
+dydT(4)=(k*Ca2)+((F*Cb1)/v)-((F*Cb2)/v);
+
 dydT=dydT';
 end
